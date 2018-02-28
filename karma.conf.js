4c0e8e2f7df134288f75282184aae74f9df97533
Samuel Ryzycki <samuel@ryzycki.com>
2018-02-28 17:07:59 +0000

    fdsaf saf sa fasf

--- karma.conf.js
+++ karma.conf.js
@@ -27,13 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'ChromeHeadlessNoSandbox' ],
-    customLaunchers         : {
-      ChromeHeadlessNoSandbox: {
-        base : 'ChromeHeadless',
-        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
-      }
-    },
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
